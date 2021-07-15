df006f001a94aa0e4818ae78910682a9ff5e2417
AnthonyArmour <anthonydarmour@gmail.com>
2021-07-15 17:05:58 +0000

    adding task 3

---
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def matrix_transpose(matrix):
+    """transposes a 2d matrix"""
+    mTransposed = [[] for x in range(len(matrix[0]))]
+    for x, arr in enumerate(mTransposed):
+        for row in matrix:
+            arr.append(row[x])
+    return mTransposed
