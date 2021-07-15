f057fcffcb5fb0899911b50f19df83d5581fec36
AnthonyArmour <anthonydarmour@gmail.com>
2021-07-15 17:21:06 +0000

    adding task 4

---
+++ math/0x00-linear_algebra/4-line_up.py
@@ -0,0 +1,10 @@
+#!/bin/usr/env python3
+
+def add_arrays(arr1, arr2):
+    """adds arrays together"""
+    if len(arr1) != len(arr2):
+        return None
+    sum_array = []
+    for x in range(len(arr1)):
+        sum_array.append(arr1[x] + arr2[x])
+    return sum_array
