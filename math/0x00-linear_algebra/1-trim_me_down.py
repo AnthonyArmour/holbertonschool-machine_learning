bb7aa60e8f41e7f7ef50818ad8942b59f840d5a9
AnthonyArmour <anthonydarmour@gmail.com>
2021-07-15 16:27:07 +0000

    chmod task 1

--- math/0x00-linear_algebra/1-trim_me_down.py
+++ math/0x00-linear_algebra/1-trim_me_down.py
@@ -2,5 +2,5 @@
 matrix = [[1, 3, 9, 4, 5, 8], [2, 4, 7, 3, 4, 0], [0, 3, 4, 6, 1, 5]]
 the_middle = []
 for arr in matrix:
-    the_middle.append(arr[2:4])
+    the_middle.append(arr[2:4].copy())
 print("The middle columns of the matrix are: {}".format(the_middle))
