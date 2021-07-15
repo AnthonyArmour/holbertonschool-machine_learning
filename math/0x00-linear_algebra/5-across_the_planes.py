b01bd84252f757d0d96c6b2c8f9eddc0e0d00c8a
AnthonyArmour <anthonydarmour@gmail.com>
2021-07-15 18:18:34 +0000

    debug task 5

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -2,22 +2,9 @@
 """adds two matrices"""
 
 
-def matrix_shape(matrix):
-    """finds dimensions of a matrix"""
-    mat = matrix
-    dim = []
-    while True:
-        try:
-            dim.append(len(mat))
-            mat = mat[0]
-        except TypeError:
-            break
-    return dim
-
-
 def add_matrices2D(mat1, mat2):
     """adds 2d matrices"""
-    if matrix_shape(mat1) != matrix_shape(mat2):
+    if len(mat1) != len(mat2):
         return None
     sum_matrix = [[] for x in range(len(mat1))]
     for x, row in enumerate(mat1):
