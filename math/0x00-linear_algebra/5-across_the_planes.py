4f3ff6db8b2260e4f3052f5b272f16c59bb41f8a
AnthonyArmour <anthonydarmour@gmail.com>
2021-07-15 18:29:49 +0000

    debug task 5

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -6,6 +6,9 @@ def add_matrices2D(mat1, mat2):
     """adds 2d matrices"""
     if len(mat1) != len(mat2):
         return None
+    for x, row in enumerate(mat1):
+        if len(row) != len(mat2[x]):
+            return None
     sum_matrix = [[] for x in range(len(mat1))]
     for x, row in enumerate(mat1):
         for y, item in enumerate(mat2[x]):
