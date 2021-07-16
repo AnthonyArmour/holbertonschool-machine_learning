32a702efa2f2e31301a672a767511b6de6ca41f5
AnthonyArmour <anthonydarmour@gmail.com>
2021-07-16 00:43:18 +0000

    adding task 12

---
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""add sub mul div matrices"""
+
+
+def np_elementwise(mat1, mat2):
+    """add sub mul div matrices"""
+    return mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2
