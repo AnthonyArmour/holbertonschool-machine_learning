c7c93fe495bb38ff2b6645f290df1dd50aaf1c9d
AnthonyArmour <anthonydarmour@gmail.com>
2021-07-23 21:57:02 +0000

    debug task 10

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -4,7 +4,7 @@
 
 def poly_derivative(poly):
     """finds derivative of polynomial"""
-    if len(poly) < 2:
+    if type(poly) is not list or len(poly) < 2:
         return [0]
     poly_prime = [num * i for i, num in enumerate(poly[1:], start=1)]
     return poly_prime
