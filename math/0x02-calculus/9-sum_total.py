4c0b5d2936615a56001228a979b0b1990617d116
AnthonyArmour <anthonydarmour@gmail.com>
2021-07-23 21:55:13 +0000

    another debug for task 9

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -5,6 +5,6 @@
 def summation_i_squared(n):
     """calculates sigma"""
     sum = 0
-    if type(n) is not int:
+    if type(n) is not int or n < 1:
         return None
     return int((n * (n + 1) * ((2 * n) + 1)) / 6)
