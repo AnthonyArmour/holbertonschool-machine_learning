b5c52260f3eb3ac64e8fb76856346d88ce0dbbcf
AnthonyArmour <anthonydarmour@gmail.com>
2021-08-07 21:01:46 +0000

    added edge case protection for task 24

--- supervised_learning/0x01-classification/24-one_hot_encode.py
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -13,6 +13,10 @@ def one_hot_encode(Y, classes):
        One hot encode function to be used to reshape
        Y_label vector
     """
+    if type(Y) is not np.ndarray or type(classes) is not int:
+        return None
+    if classes < 2 or classes < np.amax(Y):
+        return None
     mat_encode = np.zeros((len(Y), classes))
     for x, label in enumerate(Y):
         mat_encode[x, label] = 1
