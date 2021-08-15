e028e227e3ab74a73118510ec92837e51622ac50
AnthonyArmour <anthonydarmour@gmail.com>
2021-08-15 03:12:19 +0000

    debug test

--- supervised_learning/0x00-tensorflow/3-calculate_accuracy.py
+++ supervised_learning/0x00-tensorflow/3-calculate_accuracy.py
@@ -7,6 +7,9 @@ import tensorflow as tf
 
 def calculate_accuracy(y, y_pred):
     """Calculates accuracy of prediction"""
+    sess = tf.Session()
+    print("y: {}".format(sess.run(y)))
+    print("y_pred: {}".format(sess.run(y_pred)))
     equality = tf.math.equal(y_pred, y)
     accuracy = tf.math.reduce_mean(tf.cast(equality, tf.float32))
     return accuracy
