c96e674054f789a6e75612f35be7a1833bba81ad
AnthonyArmour <anthonydarmour@gmail.com>
2021-08-15 02:47:23 +0000

    debug task 1 pystyle

--- supervised_learning/0x00-tensorflow/1-create_layer.py
+++ supervised_learning/0x00-tensorflow/1-create_layer.py
@@ -6,6 +6,7 @@ import tensorflow as tf
 
 
 def create_layer(prev, n, activation):
+    """creates layer for NN in Tensorflow"""
     weights = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
     return tf.layers.dense(prev, n, activation=activation,
                            kernel_initializer=weights)
