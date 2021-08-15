1c41957fe1b0cd1642495af32a446a2035c0ccd6
AnthonyArmour <anthonydarmour@gmail.com>
2021-08-15 02:49:01 +0000

    pushing task2

--- supervised_learning/0x00-tensorflow/2-forward_prop.py
+++ supervised_learning/0x00-tensorflow/2-forward_prop.py
@@ -7,12 +7,10 @@ import tensorflow as tf
 
 create_layer = __import__('1-create_layer').create_layer
 
+
 def forward_prop(x, layer_sizes=[], activations=[]):
     """Forward_prop using tensorflow"""
     pred = x
     for i in range(len(layer_sizes)):
-        # with tf.variable_scope("layer", reuse=tf.AUTO_REUSE):
         pred = create_layer(pred, layer_sizes[i], activations[i])
-        # else:
-        #     pred = create_layer(pred, layer_sizes[i], activations[i])
     return pred
