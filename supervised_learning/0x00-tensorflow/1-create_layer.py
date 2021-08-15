8735b65b39bd24a23cf59a5f0dab53d6c874e477
AnthonyArmour <anthonydarmour@gmail.com>
2021-08-15 02:46:10 +0000

    debug try

--- supervised_learning/0x00-tensorflow/1-create_layer.py
+++ supervised_learning/0x00-tensorflow/1-create_layer.py
@@ -8,4 +8,4 @@ import tensorflow as tf
 def create_layer(prev, n, activation):
     weights = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
     return tf.layers.dense(prev, n, activation=activation,
-                           kernel_initializer=weights, name="layer")
+                           kernel_initializer=weights)
