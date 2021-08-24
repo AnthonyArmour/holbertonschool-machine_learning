ed1b4c69c6d6af8c0c476c474bad6469f6498d1e
AnthonyArmour <anthonydarmour@gmail.com>
2021-08-24 17:13:53 +0000

    p

--- supervised_learning/0x01-optimization/15-model.py
+++ supervised_learning/0x01-optimization/15-model.py
@@ -35,7 +35,8 @@ def create_batch_norm_layer(prev, n, activation, last, epsilon):
          A tensor of the activated output for the layer.
     """
     weights = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    Z = tf.layers.dense(prev, n, kernel_initializer=weights)
+    Z = tf.layers.dense(n, kernel_initializer=weights)
+    Z = Z(prev)
     if last is True:
         return Z
     mean, variance = tf.nn.moments(Z, 0)
