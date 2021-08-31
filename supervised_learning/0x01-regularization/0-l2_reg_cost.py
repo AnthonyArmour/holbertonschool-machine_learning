1e493f839c2a9965b9b0ab99e3332933c162a77d
AnthonyArmour <anthonydarmour@gmail.com>
2021-08-31 16:57:03 +0000

    pushing task 0

---
+++ supervised_learning/0x01-regularization/0-l2_reg_cost.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+   Module contains
+   l2_reg_cost(cost, lambtha, weights, L, m):
+"""
+
+
+import numpy as np
+
+
+def l2_reg_cost(cost, lambtha, weights, L, m):
+    """
+       Calculates the cost of a neural network with L2 regularization.
+
+       Args:
+         cost: Cost of the network without L2 regularization
+         lambtha: The regularization parameter
+         weights: Dictionary of the weights and biases
+         L: Number of layers in network
+         m: Number of data points used
+       Returns:
+         The cost of the network accounting for L2.
+    """
+    norm = 0
+    for w in weights.values():
+        norm += np.sum((w**2).flatten())
+    L2 = (lambtha/(2*m)) * norm
+    return cost + L2
---
+++ supervised_learning/0x01-regularization/README.md
@@ -0,0 +1,8 @@
+# Neural Network Regularization Project
+
+## Covers L2 regularization, dropout gradient descent and early stopping
+
+* l2_reg_cost(cost, lambtha, weights, L, m):
+
+> Calculates the cost of a neural network with L2 regularization.
+
