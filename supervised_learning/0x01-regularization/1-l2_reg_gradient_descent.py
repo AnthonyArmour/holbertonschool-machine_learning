37a4aa310fc8c2f8790307e6b2cb8c9e49d73a30
AnthonyArmour <anthonydarmour@gmail.com>
2021-08-31 17:38:31 +0000

    debug task 1

--- supervised_learning/0x01-regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/0x01-regularization/1-l2_reg_gradient_descent.py
@@ -29,7 +29,8 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     for x in range(L, 0, -1):
         l2w = ((alpha*lambtha)/m)
         dw = alpha * (np.matmul(dZL, cache["A{}".format(x-1)].T))
-        weights["W{}".format(x)] -= l2w - dw
         weights["b{}".format(x)] = np.sum(dZL, axis=1, keepdims=True)/m
         if x != 1:
-            dZL = cache["A{}".format(x-1)] * (1 - cache["A{}".format(x-1)])
+            dz = cache["A{}".format(x-1)] * (1 - cache["A{}".format(x-1)])
+            dZL = np.matmul(weights["W{}".format(x)].T, dZL) * dz
+        weights["W{}".format(x)] -= l2w - dw
