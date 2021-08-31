96c7124735f268b94696473c93783b8fbeca6405
AnthonyArmour <anthonydarmour@gmail.com>
2021-08-31 19:26:16 +0000

    debug task 4

--- supervised_learning/0x01-regularization/4-dropout_forward_prop.py
+++ supervised_learning/0x01-regularization/4-dropout_forward_prop.py
@@ -11,7 +11,7 @@ import numpy as np
 def activate(layer, W, A, softmax=False):
     """
        Tanh method for hidden layers.
-       
+
        Args:
          layer: layer to activate
            W: weights and biases dictionary
@@ -49,7 +49,9 @@ def dropout_forward_prop(X, weights, L, keep_prob):
     for i in range(1, L+1):
         if i != L:
             a = activate(i, weights, A, softmax=False)
-            mask = (np.random.rand(a.shape[0], a.shape[1]) < keep_prob).astype(int)
+            mask = (
+                np.random.rand(a.shape[0], a.shape[1]) < keep_prob
+                ).astype(int)
             A["A{}".format(i)] = (a*mask)/keep_prob
             A["D{}".format(i)] = mask
         else:
