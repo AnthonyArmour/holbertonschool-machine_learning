d38a0ae67f79cfab6abfb1ac7b26a8f6cb7b36a4
AnthonyArmour <anthonydarmour@gmail.com>
2021-09-02 19:48:52 +0000

    pushing task 5

---
+++ supervised_learning/0x02-keras/5-train.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+   Module contains
+   train_model(network, data, labels, batch_size,
+     epochs, validation_data=None, verbose=True,
+     shuffle=False):
+"""
+
+
+import tensorflow.keras as k
+
+
+def train_model(
+        network, data, labels, batch_size, epochs,
+        validation_data=None, verbose=True, shuffle=False):
+    """
+       Trains a model using mini-batch gradient descent.
+
+       Args:
+         network: The model to optimize
+         data: numpy.ndarray - input data
+         labels: One hot matrix containing Y data
+         batch_size: Size of batches for mini
+           batch gradient descent
+         epochs: Number of epochs to train
+         verbose: Boolean determining print status
+         shuffle: boolean whether to shuffle data in order
+
+        Returns:
+          History object.
+    """
+    return network.fit(
+        x=data, y=labels, batch_size=batch_size,
+        epochs=epochs, verbose=verbose,
+        validation_data=validation_data, shuffle=shuffle
+        )
