0ff6a677d8a0ffa0adfa64228f8d0b9d98a7bd6a
AnthonyArmour <anthonydarmour@gmail.com>
2021-09-08 20:23:23 +0000

    pushing task 6

---
+++ math/0x03-convolutions_and_pooling/6-pool.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+   Module contains
+   pool(images, kernel_shape, stride, mode='max'):
+"""
+
+
+import numpy as np
+
+
+def pool(images, kernel_shape, stride, mode='max'):
+    """
+       Performs pooling on images.
+
+       Args:
+         images: numpy.ndarray - (m, h, w, c) contains multiple
+           greyscale images
+         kernel_shape: numpy.ndarray - (kh, kw) kernels for convolution
+         stride: (sh, sw) tuple of stride dimensions
+         mode: type of pooling
+
+       Returns:
+         numpy.ndarray - convolved images
+    """
+    samples, samp_h, samp_w, samp_c = images.shape
+    filter_h, filter_w = kernel_shape
+    sh, sw = stride
+
+    if mode == "max":
+        op = np.amax
+    else:
+        op = np.average
+
+    pad_h, pad_w = 0, 0
+
+    out_h = (samp_h + (2 * pad_h) - filter_h) // sh + 1
+    out_w = (samp_w + (2 * pad_w) - filter_w) // sw + 1
+
+    conv = np.zeros((samples, out_h, out_w, samp_c))
+
+    for h in range(out_h):
+        for w in range(out_w):
+            conv[:, h, w, :] = op(images[:, sh*h: sh*h+filter_h, sw*w:sw*w+filter_w, :], axis=(1, 2))
+    return conv
--- math/0x03-convolutions_and_pooling/README.md
+++ math/0x03-convolutions_and_pooling/README.md
@@ -5,3 +5,27 @@
 * convolve_grayscale_valid(images, kernel):
 
 > Performs a valid convolution on grayscale images.
+
+* convolve_grayscale_same(images, kernel): 
+
+> Performs a valid convolution on grayscale images.
+
+* convolve_grayscale_padding(images, kernel, padding):
+
+> Performs a valid convolution on grayscale images with custom padding.
+
+* convolve_grayscale(images, kernel, padding='same', stride=(1, 1)): 
+
+> Performs a valid convolution on grayscale images.
+
+* convolve_channels(images, kernel, padding='same', stride=(1, 1)):
+
+> Performs a convolution on images with channels.
+
+* convolve(images, kernels, padding='same', stride=(1, 1)):
+
+> Performs a convolution on images using multiple kernels.
+
+* pool(images, kernel_shape, stride, mode='max'):
+
+> Performs pooling on images.
