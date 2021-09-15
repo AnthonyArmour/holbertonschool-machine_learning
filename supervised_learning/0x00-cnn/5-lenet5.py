438ea56422efd5b6045edeed8083af64f83f5036
AnthonyArmour <anthonydarmour@gmail.com>
2021-09-15 19:19:16 +0000

    pushing task 5

---
+++ supervised_learning/0x00-cnn/5-lenet5.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""
+   Module contains
+   lenet5(X):
+"""
+
+
+from typing import AbstractSet
+import tensorflow.keras as K
+
+
+def lenet5(X):
+    """
+       Modified version of the LeNet-5 architecture using keras.
+
+       Args:
+         X: K.Input - (m, 28, 28, 1) input images
+
+       Return:
+         tensor for softmax activation
+         Adam training operation
+         loss tensor
+         accuracy tensor
+    """
+    init = K.initializers.HeNormal()
+    conv1 = K.layers.Conv2D(
+        filters=6, kernel_size=(5, 5), padding='same',
+        activation='relu', kernel_initializer=init
+    )(X)
+    pool1 = K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv1)
+
+    conv2 = K.layers.Conv2D(
+        filters=16, kernel_size=(5, 5), padding='valid',
+        activation='relu', kernel_initializer=init
+    )(pool1)
+    pool2 = K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv2)
+
+    flat = K.layers.Flatten()(pool2)
+
+    fc1 = K.layers.Dense(
+        units=120, kernel_initializer=init, activation='relu'
+        )(flat)
+
+    fc2 = K.layers.Dense(
+        units=84, kernel_initializer=init, activation='relu'
+        )(fc1)
+
+    fc3 = K.layers.Dense(
+        units=10, kernel_initializer=init, activation='softmax'
+        )(fc2)
+
+    model = K.models.Model(X, fc3)
+
+    model.compile(optimizer=K.optimizers.Adam(), metrics=['accuracy'])
+    return model
--- supervised_learning/0x00-cnn/README.md
+++ supervised_learning/0x00-cnn/README.md
@@ -8,3 +8,19 @@
 * pool_forward(A_prev, kernel_shape, stride=(1, 1), mode='max'):
 
 > Performs forward propagation over a pooling layer of a neural network.
+
+* conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
+
+> Performs back propagation over a convolutional layer of a neural network.
+
+* pool_backward(dA, A_prev, kernel_shape, stride=(1, 1), mode='max'):
+
+> Performs back propagation over a pooling layer of a neural network.
+
+* lenet5(x, y):
+
+> Modified version of the LeNet-5 architecture using tensorflow.
+
+* lenet5(X):
+
+> Modified version of the LeNet-5 architecture using keras.
