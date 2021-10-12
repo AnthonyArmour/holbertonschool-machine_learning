ba86bbfae80b08cf4502f07c4be496871edd9d80
AnthonyArmour <anthonydarmour@gmail.com>
2021-10-12 17:00:40 +0000

    pushing task 2

---
+++ supervised_learning/0x00-object_detection/2-yolo.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python3
+"""
+   Module contains:
+   Class Yolo
+"""
+
+
+from numpy.core.fromnumeric import searchsorted
+import tensorflow.keras as K
+import tensorflow.keras.backend as backend
+from numpy import concatenate as cat
+import tensorflow as tf
+import numpy as np
+
+
+class Yolo():
+    """
+       Yolo v3 class for performing object detection.
+
+       Public Instance Attributes
+            model: the Darknet Keras model
+            class_names: a list of the class names for the model
+            class_t: the box score threshold for the initial filtering step
+            nms_t: the IOU threshold for non-max suppression
+            anchors: the anchor boxes
+    """
+
+    def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
+        """
+           Init method for instanciating Yolo class.
+
+           Args:
+             model_path: path to Darknet keras model
+             classes_path: path to list of class names for
+               darknet model
+             class_t: float representing box score for initial
+               filtering step
+             nms_t: float representing IOU threshold for non-max
+               supression
+             anchors: numpy.ndarray - shape (outputs, anchor_boxes, 2)
+               containing all anchor boxes
+                 outputs: number of predictions made
+                 anchor_boxes: number of anchor boxes for each pred.
+                 2 => [anchor_box_width, anchor_box_height]
+        """
+        self.model = K.models.load_model(model_path)
+        with open(classes_path, 'rt') as fd:
+            self.class_names = fd.read().rstrip('\n').split('\n')
+        self.class_t = class_t
+        self.nms_t = nms_t
+        self.anchors = anchors
+
+    def sigmoid(self, arr):
+        """sigmoid activation function"""
+        return 1 / (1+np.exp(-1*arr))
+
+    def process_outputs(self, outputs, image_size):
+        """
+           Args:
+             outputs: numpy.ndarray - contains predictions from model
+               for single image.
+             image_size: numpy.ndarray - images original
+               size (image_height, image_width)
+
+           Return:
+              tuple - (boxes, box_confidence, box_class_probs)
+              boxes: numpy.ndarray - (grid_height, grid_width, anchorboxes, 4)
+                 4 => (x1, y1, x2, y2)
+              box_confidence: numpy.ndarray - shape
+                (grid_height, grid_width, anchor_boxes, 1)
+              box_class_probs: numpy.ndarray - shape
+                (grid_height, grid_width, anchor_boxes, classes)
+                contains class probabilities for each output
+        """
+        IH, IW = image_size[0], image_size[1]
+        boxes = [output[..., :4] for output in outputs]
+        box_confidence, class_probs = [], []
+        cornersX, cornersY = [], []
+
+        for output in outputs:
+            # Organize grid cells
+            gridH, gridW, anchors = output.shape[:3]
+            cx = np.arange(gridW).reshape(1, gridW)
+            cx = np.repeat(cx, gridH, axis=0)
+            cy = np.arange(gridW).reshape(1, gridW)
+            cy = np.repeat(cy, gridH, axis=0).T
+
+            cornersX.append(
+                np.repeat(cx[..., np.newaxis], anchors, axis=2)
+                )
+            cornersY.append(
+                np.repeat(cy[..., np.newaxis], anchors, axis=2)
+                )
+            # box confidence and class probability activations
+            box_confidence.append(self.sigmoid(output[..., 4:5]))
+            class_probs.append(self.sigmoid(output[..., 5:]))
+
+        inputW = self.model.input.shape[1].value
+        inputH = self.model.input.shape[2].value
+
+        # Predicted boundary box
+        for x, box in enumerate(boxes):
+            bx = (
+                (self.sigmoid(box[..., 0])+cornersX[x])/outputs[x].shape[1]
+                )
+            by = (
+                (self.sigmoid(box[..., 1])+cornersY[x])/outputs[x].shape[0]
+                )
+            bw = (
+                (np.exp(box[..., 2])*self.anchors[x, :, 0])/inputW
+                )
+            bh = (
+                (np.exp(box[..., 3])*self.anchors[x, :, 1])/inputH
+                )
+
+            # x1
+            box[..., 0] = (bx - (bw * 0.5))*IW
+            # y1
+            box[..., 1] = (by - (bh * 0.5))*IH
+            # x2
+            box[..., 2] = (bx + (bw * 0.5))*IW
+            # y2
+            box[..., 3] = (by + (bh * 0.5))*IH
+
+        return (boxes, box_confidence, class_probs)
+
+    def filter_boxes(self, boxes, box_confidences, box_class_probs):
+        """
+           Args:
+            boxes: numpy.ndarrays - (grid_height, grid_width, anchor_boxes, 4)
+              containing the processed boundary boxes for
+              each output, respectively
+            box_confidences: a list of numpy.ndarrays of shape
+              (grid_height, grid_width, anchor_boxes, 1) containing
+              the processed box confidences for each output, respectively
+            box_class_probs: a list of numpy.ndarrays of shape
+              (grid_height, grid_width, anchor_boxes, classes) containing the
+              processed box class probabilities for each output, respectively
+
+           Return:
+            filtered_boxes: a numpy.ndarray of shape (?, 4) containing
+              all of the filtered bounding boxes
+            box_classes: a numpy.ndarray of shape (?,) containing the class
+              number that each box in filtered_boxes predicts, respectively
+            box_scores: a numpy.ndarray of shape (?) containing the box scores
+              for each box in filtered_boxes, respectively
+        """
+        best_boxes, scores, classes = None, None, None
+        for x in range(len(boxes)):
+            box_score = box_confidences[x] * box_class_probs[x]
+            box_class = np.argmax(box_score, axis=-1)
+            box_score = np.amax(box_score, axis=-1)
+            mask = box_score >= self.class_t
+
+            if best_boxes is None:
+                best_boxes = boxes[x][mask]
+                scores = box_score[mask]
+                classes = box_class[mask]
+            else:
+                best_boxes = cat((best_boxes, boxes[x][mask]), axis=0)
+                scores = cat((scores, box_score[mask]), axis=0)
+                classes = cat((classes, box_class[mask]), axis=0)
+
+        return (best_boxes, classes, scores)
