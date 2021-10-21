88acbe4b66a951c0a0b7c7fe465eab199b34ba4d
AnthonyArmour <anthonydarmour@gmail.com>
2021-10-21 19:05:06 +0000

    pushing task 3

--- supervised_learning/0x01-face_verification/align.py
+++ supervised_learning/0x01-face_verification/align.py
@@ -24,3 +24,28 @@ class FaceAlign():
         """
         self.detector = dlib.get_frontal_face_detector()
         self.shape_predictor = dlib.shape_predictor(shape_predictor_path)
+
+    def detect(self, image):
+        """
+           Detects a face in an image.
+
+           Args:
+            image: rank 3 np.ndarray - containing image
+
+           Return:
+            dlib.rectangle - containing boundary box or None.
+                If multiple faces are detected, return the dlib.rectangle
+                  with the largest area.
+                If no faces are detected, return a dlib.rectangle that
+                  is the same as the image.
+        """
+        def area(x):
+            """gets area of box"""
+            return (x.right()-x.left())*(x.bottom()-x.top())
+
+        dets = self.detector(image, 1)
+        if len(dets):
+            dets = sorted(dets, key=lambda x: area(x), reverse=True)
+            return dets[0]
+        else:
+            return dlib.rectangle(0, 0, image.shape[-2], image.shape[-3])
