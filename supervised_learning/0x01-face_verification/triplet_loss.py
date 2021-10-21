7b89959e503b348cfd26767db425931483a2f6b1
AnthonyArmour <anthonydarmour@gmail.com>
2021-10-21 23:49:35 +0000

    pushing trip loss

--- supervised_learning/0x01-face_verification/triplet_loss.py
+++ supervised_learning/0x01-face_verification/triplet_loss.py
@@ -3,6 +3,7 @@
 
 
 import numpy as np
+import tensorflow as tf
 import tensorflow.keras as K
 
 
@@ -13,3 +14,19 @@ class TripletLoss(K.layers.Layer):
         super().__init__(**kwargs)
         """Class constructor"""
         self.alpha = alpha
+
+    def triplet_loss(self, inputs):
+        """
+           Computes triplet loss
+
+           Args:
+            inputs: list containing the anchor, positive
+              and negative output tensors from the last
+              layer of the model, respectively.
+
+           Return:
+            tensor containing losses.
+        """
+        dist_ap = np.linalg.norm(inputs[0]-inputs[1], axis=1)
+        dist_an = np.linalg.norm(inputs[0]-inputs[2], axis=1)
+        return tf.maximum(dist_ap - dist_an + self.alpha, 0.0)
