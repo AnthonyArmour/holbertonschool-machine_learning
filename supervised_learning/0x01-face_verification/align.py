222876fb9b31fb94075262f7170b116c1d3c11b5
AnthonyArmour <anthonydarmour@gmail.com>
2021-10-21 18:33:32 +0000

    pushing align class file

---
+++ supervised_learning/0x01-face_verification/align.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+   Module contains class FaceAlign
+"""
+
+
+import numpy as np
+import dlib
+
+
+class FaceAlign():
+
+    def __init__(self, shape_predictor_path):
+        """
+           Class Constructor
+
+           Args:
+            shape_predictor_path: path to dlib shape predictor model.
+
+           Attributes:
+            detector: contains dlib's default face detector
+            shape_predictor: contains dlib.shape_predictor
+        """
+        self.detector = dlib.get_frontal_face_detector()
+        self.shape_predictor = dlib.shape_predictor(shape_predictor_path)
