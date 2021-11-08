f27ac415d502612754b37d2321c61fe940965ab7
AnthonyArmour <anthonydarmour@gmail.com>
2021-11-08 01:59:09 +0000

    pushong task 5

---
+++ math/0x00-advanced_linear_algebra/5-definiteness.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""Matrix Definitness Module"""
+
+
+import numpy as np
+
+
+def definiteness(matrix):
+    """
+       Computes definitness of a matrix.
+
+       Args:
+         matrix: Matrix to compute.
+
+       Return:
+         Definitness of a matrix.
+    """
+
+    try:
+        eig, _ = np.linalg.eig(matrix)
+
+        pos = (eig > 0)
+        neg = (eig < 0)
+
+        if False not in pos:
+            return "Positive definite"
+        elif False not in neg:
+            return "Negative definite"
+        elif True in pos and True not in neg:
+            return "Positive semi-definite"
+        elif True in neg and True not in pos:
+            return "Negative semi-definite"
+        return "Indefinite"
+    except Exception:
+        return None
