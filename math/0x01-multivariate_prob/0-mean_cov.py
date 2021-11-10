52038d9feb1eb0e193a90719bdef9804893eefc2
AnthonyArmour <anthonydarmour@gmail.com>
2021-11-10 16:53:07 +0000

    d

--- math/0x01-multivariate_prob/0-mean_cov.py
+++ math/0x01-multivariate_prob/0-mean_cov.py
@@ -26,7 +26,7 @@ def mean_cov(X):
     if type(X) is not np.ndarray or len(X.shape) != 2:
         raise TypeError("X must be a 2D numpy.ndarray")
 
-    if X.shape[1] < 2:
+    if X.shape[1] < 2 or X.shape[0] < 2:
         raise ValueError("X must contain multiple data points")
 
     mean = X.sum(axis=0)/X.shape[0]
