f2771aa36ca5abdcccc48afba141f2655816f4e5
AnthonyArmour <anthonydarmour@gmail.com>
2021-11-23 01:46:06 +0000

    pushing task 3

--- unsupervised_learning/0x01-clustering/3-optimum.py
+++ unsupervised_learning/0x01-clustering/3-optimum.py
@@ -9,7 +9,7 @@ clusters by variance.
 import numpy as np
 
 
-def optimum(X, kmin=1, kmax=None, iterations=1000):
+def optimum_k(X, kmin=1, kmax=None, iterations=1000):
     """
     Tests for the optimum number of clusters by variance.
 
