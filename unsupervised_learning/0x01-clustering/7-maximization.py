b5409c7e75ea7f237651c8adb11b9c5fd30b90e7
AnthonyArmour <anthonydarmour@gmail.com>
2021-11-24 00:19:50 +0000

    pushing task 7

---
+++ unsupervised_learning/0x01-clustering/7-maximization.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+"""
+Module contains function for calculating
+the maximization step in the EM algorithm
+for a Gaussian Mixture Model.
+"""
+
+
+import numpy as np
+
+
+def cov(X, P, mean):
+    """
+    Calculates the covariance of a dataset, given the mean.
+
+    Args:
+        X: numpy.ndarray - shape (n, d) containing data set.
+            n: Number of data points.
+            d: Number of dimensions in each data point.
+        P: numpy.ndarray - (n, 1) Normalizer equivalent to
+        the likelihood of rows X given a gaussian C with given mean,
+        over n * the prior of C.
+        belonging to gaussian with mean "mean".
+        mean: numpy.ndarray - shape (d,) mean for calculation.
+
+    Returns: cov
+        covariance: numpy.ndarray - shape (d, d) containing the
+            covariance matrix of the data set.
+    """
+
+    _, d = X.shape
+    deviation = X - mean
+    covariance = np.matmul(deviation.T, P*deviation)
+
+    return covariance
+
+
+def maximization(X, g):
+    """
+    Calculates the maximization step in the EM algorithm
+    for a Gaussian Mixture Model.
+
+    Args:
+        X: numpy.ndarray - (n, d) Data set.
+        g: numpy.ndarray - (k, n) Posterior probabilities for
+        each data point in each cluster.
+
+    Return: priors, m, S, or None, None, None on failure
+        priors: numpy.ndarray - (k,) Updated priors for each cluster.
+        means: numpy.ndarray - (k, d) Updated centroid means for
+        each cluster.
+        S: numpy.ndarray - (k, d, d) Updated covariance matrices
+        for each cluster.
+    """
+
+    k, n = g.shape
+
+    priors = g.sum(axis=1)
+
+    means = (g.reshape(k, n, 1)*X).sum(axis=1)
+    means = means/priors[np.newaxis, ...].T
+
+    # Covariance matrices
+    S = []
+
+    for i in range(k):
+        S.append(cov(X, g[i][np.newaxis, ...].T/priors[i], means[i]))
+
+    return priors/n, means, np.array(S)
