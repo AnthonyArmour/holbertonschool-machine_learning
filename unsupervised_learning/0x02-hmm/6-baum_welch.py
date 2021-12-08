f65a055e8250fa6417baf6c64306aebf6477bd2e
AnthonyArmour <anthonydarmour@gmail.com>
2021-12-08 00:15:14 +0000

    removed imports and added previously made functions for checker

--- unsupervised_learning/0x02-hmm/6-baum_welch.py
+++ unsupervised_learning/0x02-hmm/6-baum_welch.py
@@ -7,12 +7,112 @@ transition and emission probabilities for a Hidden Markov Model.
 
 
 import numpy as np
-# The forward algorithm for computing the probabilies
-# of being in a hidden state gievn all previous observations.
-forward = __import__('3-forward').forward
-# Backward algo. gives probabilities of being in hidden state,
-# given all future observations.
-backward = __import__('5-backward').backward
+
+
+def forward(Observation, Emission, Transition, Initial):
+    """
+    Performs the forward algorithm for a hidden markov model.
+
+    Args:
+        Observation: numpy.ndarray - (T,) that contains the index of
+        the observation.
+            - T: Number of observations.
+        Emission: numpy.ndarray - (N, M) Emission probability of a specific
+        observation given a hidden state.
+            - Emission[i, j]: Probability of observing j given the hidden
+            state i.
+            - N: Number of hidden states.
+            - M: Number of all possible observations.
+        Transition: numpy.ndarray - (N, N) Transition probabilities.
+            - Transition[i, j]: Probability of transitioning from the hidden
+            state i to j.
+        Initial: numpy.ndarray - (N, 1) Probability of starting in a particular
+        hidden state.
+
+    Return: P, F, or None, None on failure
+        P: Likelihood of the observations given the model.
+        F: numpy.ndarray of shape (N, T) containing the forward path
+        probabilities.
+            - F[i, j]: Probability of being in hidden state i at time j given
+            the previous observations.
+    """
+
+    if type(Observation) is not np.ndarray or Observation.ndim != 1:
+        return None, None
+    if Observation.shape[0] == 0:
+        return None, None
+    if type(Emission) is not np.ndarray or Emission.ndim != 2:
+        return None, None
+    if type(Transition) is not np.ndarray or Transition.ndim != 2:
+        return None, None
+    if type(Initial) is not np.ndarray or len(Initial) != Transition.shape[0]:
+        return None, None
+
+    n, m = Emission.shape
+
+    # N x 1 (time step 0)
+    F = (Initial.T * Emission[:, Observation[0]]).T
+
+    for t in range(1, Observation.shape[0]):
+        rt = F[:, t-1].T * Transition.T.reshape(n, 1, n)
+        rt = rt * Emission[:, Observation[t]].reshape(n, 1, 1)
+        F = np.concatenate((F, rt.sum(-1)), axis=1)
+
+    return np.sum(F[:, -1]), F
+
+
+def backward(Observation, Emission, Transition, Initial):
+    """
+    Performs the backward algorithm for a hidden markov model.
+
+    Args:
+        Observation:numpy.ndarray - (T,) that contains the index of the
+        observation.
+            T: Number of observations.
+        Emission: numpy.ndarray - (N, M) Emission probabilities of a specific
+        observation given a hidden state.
+            Emission[i, j]: Probability of observing j given the hidden
+            state i.
+            N: Number of hidden states.
+            M: Number of all possible observations.
+        Transition: 2D numpy.ndarray - (N, N) Transition probabilities.
+            Transition[i, j] Probability of transitioning from the hidden
+            state i to j.
+        Initial a numpy.ndarray - (N, 1) Probability of starting in a
+        particular hidden state.
+
+    Return: P, B, or None, None
+        P: Likelihood of the observations given the model.
+        B: numpy.ndarray - (N, T) Backward path probabilities.
+        B[i, j]: Probability of generating the future observations from
+        hidden state i at time j.
+    """
+
+    if type(Observation) is not np.ndarray or Observation.ndim != 1:
+        return None, None
+    if Observation.shape[0] == 0:
+        return None, None
+    if type(Emission) is not np.ndarray or Emission.ndim != 2:
+        return None, None
+    if type(Transition) is not np.ndarray or Transition.ndim != 2:
+        return None, None
+    if type(Initial) is not np.ndarray or len(Initial) != Transition.shape[0]:
+        return None, None
+
+    N, M = Emission.shape
+    T = Observation.size
+
+    B = np.ones((N, T), dtype="float")
+
+    for t in range(T-2, -1, -1):
+        mat = (Emission[:, Observation[t+1]] * Transition.reshape(N, 1, N))
+        mat = (B[:, t+1] * mat).reshape(N, N).sum(axis=1)
+
+        B[:, t] = mat
+
+    P = (Initial.T * Emission[:, Observation[0]] * B[:, 0])
+
+    return P.sum(axis=1)[0], B
 
 
 def EM(Observation, Transition, Emission, Initial):
