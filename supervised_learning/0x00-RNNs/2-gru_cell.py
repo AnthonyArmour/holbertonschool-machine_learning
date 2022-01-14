4232a85552e57037ae9d9879726ef7bc0f13361f
AnthonyArmour <anthonydarmour@gmail.com>
2022-01-14 19:44:07 +0000

    pushing tasks 2-8 for RNN project

--- supervised_learning/0x00-RNNs/2-gru_cell.py
+++ supervised_learning/0x00-RNNs/2-gru_cell.py
@@ -71,18 +71,21 @@ class GRUCell():
 
         H_SHAPE = h_prev.shape[1]
 
+        # Reset Gate
         dot_xr = np.dot(x_t, self.Wr[H_SHAPE:, :])
         dot_hr = np.dot(h_prev, self.Wr[:H_SHAPE, :])
         R = self.sigmoid(dot_xr + dot_hr + self.br)
 
+        # Update Gate
         dot_xz = np.dot(x_t, self.Wz[H_SHAPE:, :])
         dot_hz = np.dot(h_prev, self.Wz[:H_SHAPE, :])
         Z = self.sigmoid(dot_xz + dot_hz + self.bz)
 
+        # Candidate Hidden State
         dot_xh = np.dot(x_t, self.Wh[H_SHAPE:, :])
         dot_Rhh = np.dot(h_prev * R, self.Wh[:H_SHAPE, :])
         C = np.tanh(dot_xh + dot_Rhh + self.bh)
 
-        h_next = (Z * h_prev) + ((1 - Z) * C)
+        h_next = (1 - Z) * h_prev + Z * C
 
         return h_next, self.softmax(np.dot(h_next, self.Wy) + self.by)
