da4b1a792928c8c41c93ff28fc21f53c47672917
AnthonyArmour <anthonydarmour@gmail.com>
2022-02-05 17:58:04 +0000

    pushing word embedding project

---
+++ supervised_learning/0x00-word_embeddings/1-tf_idf.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""Module contains function for creating TF-IDF embedding."""
+
+
+from sklearn.feature_extraction.text import TfidfVectorizer as TFD
+
+
+def tf_idf(sentences, vocab=None):
+    """
+    Creates a TF-IDF embedding.
+
+    Args:
+        sentences: List of sentences to analyze.
+        vocab: List of the vocabulary words to use
+        for the analysis. If None, all words within
+        sentences should be used.
+
+    Return: embeddings, features
+        embeddings: numpy.ndarray - (s, f) Embeddings.
+            s: Number of sentences in sentences.
+            f: Number of features analyzed.
+        features: List of the features used for embeddings.
+    """
+
+    vec = TFD(use_idf=True, vocabulary=vocab)
+    X = vec.fit_transform(sentences)
+
+    embeddings = X.toarray()
+    features = vec.get_feature_names_out()
+
+    return embeddings, features
