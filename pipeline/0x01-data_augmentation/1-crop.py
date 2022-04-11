171518042c974daf0184e90bd130cdaeee0c1d90
AnthonyArmour <anthonydarmour@gmail.com>
2022-04-11 17:13:29 +0000

    resized cropped images

--- pipeline/0x01-data_augmentation/1-crop.py
+++ pipeline/0x01-data_augmentation/1-crop.py
@@ -24,6 +24,8 @@ def crop_image(image, size):
 if __name__ == "__main__":
     import tensorflow_datasets as tfds
     from tensorflow.keras.utils import save_img
+    from tensorflow.python.ops.numpy_ops import np_config
+    np_config.enable_numpy_behavior()
 
     tf.compat.v1.enable_eager_execution()
     tf.compat.v1.set_random_seed(1)
@@ -31,4 +33,6 @@ if __name__ == "__main__":
     doggies = tfds.load('stanford_dogs', split='train', as_supervised=True)
     for image, _ in doggies.shuffle(10).take(1):
         save_img("./images/before_random_crop.jpg", image)
-        save_img("./images/random_crop.jpg", crop_image(image, (200, 200, 3)))
+        cropped = crop_image(image, (200, 200, 3))
+        cropped = tf.image.resize(cropped, (image.shape[0], image.shape[1]))
+        save_img("./images/random_crop.jpg", cropped)
