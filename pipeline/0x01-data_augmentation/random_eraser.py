f24664a098813640000b0482752bc809ac8f658e
AnthonyArmour <anthonydarmour@gmail.com>
2022-04-13 19:45:41 +0000

    pushing eraser and adjusted readme

---
+++ pipeline/0x01-data_augmentation/random_eraser.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+"""
+Module contains function for randomly
+erasing patches of an image.
+"""
+
+
+import numpy as np
+
+
+def random_eraser(p=0.5, s_l=0.02, s_h=0.4, r_1=0.3, r_2=1/0.3, v_l=0, v_h=255, pixel_level=False):
+    """Randomly erases patches from an image"""
+    def eraser(input_img):
+        if input_img.ndim == 3:
+            img_h, img_w, img_c = input_img.shape
+        elif input_img.ndim == 2:
+            img_h, img_w = input_img.shape
+
+        p_1 = np.random.rand()
+
+        if p_1 > p:
+            return input_img
+
+        while True:
+            s = np.random.uniform(s_l, s_h) * img_h * img_w
+            r = np.random.uniform(r_1, r_2)
+            w = int(np.sqrt(s / r))
+            h = int(np.sqrt(s * r))
+            left = np.random.randint(0, img_w)
+            top = np.random.randint(0, img_h)
+
+            if left + w <= img_w and top + h <= img_h:
+                break
+
+        if pixel_level:
+            if input_img.ndim == 3:
+                c = np.random.uniform(v_l, v_h, (h, w, img_c))
+            if input_img.ndim == 2:
+                c = np.random.uniform(v_l, v_h, (h, w))
+        else:
+            c = np.random.uniform(v_l, v_h)
+
+        input_img[top:top + h, left:left + w] = c
+
+        return input_img
+
+    return eraser
+
+
+if __name__ == "__main__":
+    import tensorflow_datasets as tfds
+    from tensorflow.keras.utils import save_img
+    import tensorflow as tf
+
+    tf.compat.v1.enable_eager_execution()
+    tf.compat.v1.set_random_seed(0)
+
+    eraser = random_eraser()
+
+    doggies = tfds.load('stanford_dogs', split='train', as_supervised=True)
+    for image, _ in doggies.shuffle(10).take(1):
+        save_img("./images/before_eraser.jpg", image)
+        image = image.numpy()
+        image = eraser(image)
+        save_img("./images/eraser.jpg", image)
