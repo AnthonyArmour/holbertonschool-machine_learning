3303a55dbc8f4ace874b2be60c516c01980de402
AnthonyArmour <anthonydarmour@gmail.com>
2022-04-27 17:18:39 +0000

    another checker attempt

--- pipeline/0x01-apis/3-upcoming.py
+++ pipeline/0x01-apis/3-upcoming.py
@@ -8,29 +8,28 @@ data from the spacex api.
 import requests
 
 
-if __name__ == '__main__':
-    url = 'https://api.spacexdata.com/v4/launches/upcoming'
-    r = requests.get(url).json()
+if __name__ == "__main__":
 
-    dates = [launch['date_local'] for launch in r]
-    next_launch_date = sorted(dates)[0]
-    next_launch = r[dates.index(next_launch_date)]
+    base = "https://api.spacexdata.com/v4/"
 
-    name = next_launch['name']
-    date = next_launch['date_local']
+    info = requests.get(base+"launches/next").json()
 
-    rocket = next_launch['rocket']
-    rocket = requests.get(
-        'https://api.spacexdata.com/v4/rockets/{}'.format(rocket)
-    ).json()['name']
+    date = info["date_local"]
 
-    launchpad = next_launch['launchpad']
-    launchpad = requests.get(
-        'https://api.spacexdata.com/v4/launchpads/{}'.format(launchpad)
-    ).json()
-    pad_name = launchpad['name']
-    location = launchpad['locality']
+    name = info["name"]
 
-    print('{} ({}) {} - {} ({})'.format(
-        name, date, rocket, pad_name, location
-    ))
+    launchpad_id = "launchpads/"+info["launchpad"]
+    rocket_id = "rockets/"+info["rocket"]
+
+    rocket_info = requests.get(base+rocket_id).json()
+
+    launchpad_info = requests.get(base+launchpad_id).json()
+
+    rocket = rocket_info["name"]
+    launchpad = launchpad_info["name"]
+    pad_location = launchpad_info["locality"]
+
+    args = (name, date, rocket, launchpad, pad_location)
+    result = "{} ({}) {} - {} ({})".format(*args)
+
+    print(result)
