0ad7236cf8e5121ccff8fca7f5595d2ded453630
AnthonyArmour <anthonydarmour@gmail.com>
2022-05-05 18:00:57 +0000

    pushing task 4 using different route

--- pipeline/0x01-apis/4-rocket_frequency.py
+++ pipeline/0x01-apis/4-rocket_frequency.py
@@ -9,26 +9,47 @@ spacex api.
 import requests
 
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
 
-    base = "https://api.spacexdata.com/v4/"
+#     base = "https://api.spacexdata.com/v4/"
 
-    launches = requests.get(base+"launches/past").json()
+#     launches = requests.get(base+"launches/past").json()
 
-    rockets = {}
+#     rockets = {}
 
-    for info in launches:
-        if info["rocket"] in rockets.keys():
-            rockets[info["rocket"]] += 1
+#     for info in launches:
+#         if info["rocket"] in rockets.keys():
+#             rockets[info["rocket"]] += 1
+#         else:
+#             rockets[info["rocket"]] = 1
+
+#     for id in rockets.keys():
+#         rocket = requests.get(base+"rockets/"+id).json()
+#         key = rocket["name"]
+#         rockets[key] = rockets.pop(id)
+
+#     ordered = sorted(rockets.items(), key=lambda x: (-x[1], x[0]))
+
+#     for name, val in ordered:
+#         print("{}: {}".format(name, val))
+
+if __name__ == '__main__':
+    url = 'https://api.spacexdata.com/v4/launches'
+    rocket_url = 'https://api.spacexdata.com/v4/rockets/'
+    launches = requests.get(url).json()
+    rocket_launches = {}
+
+    for launch in launches:
+        rocket_id = launch['rocket']
+        rocket = requests.get(rocket_url + rocket_id).json()
+        name = rocket['name']
+
+        if name not in rocket_launches.keys():
+            rocket_launches[name] = 1
         else:
-            rockets[info["rocket"]] = 1
+            rocket_launches[name] += 1
 
-    for id in rockets.keys():
-        rocket = requests.get(base+"rockets/"+id).json()
-        key = rocket["name"]
-        rockets[key] = rockets.pop(id)
+    rocket_launches = sorted(rocket_launches.items(), key=lambda x: x[1])
 
-    ordered = sorted(rockets.items(), key=lambda x: (-x[1], x[0]))
-
-    for name, val in ordered:
-        print("{}: {}".format(name, val))
+    for k, v in reversed(rocket_launches):
+        print('{}: {}'.format(k, v))
