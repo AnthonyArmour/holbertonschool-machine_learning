c05642621945963708b54087f936ffd71ab95905
AnthonyArmour <anthonydarmour@gmail.com>
2022-05-12 18:52:11 +0000

    pushing first two tasks

---
+++ pipeline/0x00-databases/0-create_database_if_missing.sql
@@ -0,0 +1,2 @@
+-- Creates a database "db_0" if it doesn't already exist
+CREATE DATABASE IF NOT EXISTS db_0;
---
+++ pipeline/0x00-databases/1-first_table.sql
@@ -0,0 +1,5 @@
+-- Creates a table "first_table" in database
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
